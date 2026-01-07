c070c73a47db0cdac1468e6a9dc7738c44c71073
sharon-Emmanuel-sr <sharonmass75@gmail.com>
2026-01-07 14:53:15 +0530

    Update documentation for Pr

--- index.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config({
-  path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development'
-});
-
-console.log(`Application running in ${process.env.NODE_ENV} mode.`);
-console.log(`Connecting to: ${process.env.DB_URL}`);
